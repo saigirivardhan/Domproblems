78554f29b1218e044a57a20b579ef2d6054ca486
saigirivardhan <102036605+saigirivardhan@users.noreply.github.com>
2022-06-07 20:33:20 +0530

    Add files via upload

---
+++ in.js
@@ -0,0 +1,73 @@
+//DOM
+// console.log(document)
+
+// let y = document.getElementsByTagName("h1")
+// console.log(y)
+// setTimeout(change,5000)
+// function change(){
+//     y[0].style.color = "red"
+//     y[0].style.backgroundColor = "yellow"
+//     y[1].style.color = "yellow"
+//     y[1].style.backgroundColor = "green"
+//     y[1].style.display = "inline-block"
+// }
+
+// const x = document.getElementsByTagName("div")
+// console.log(x)
+// console.log(x[0])
+// console.log(x[1])
+// setTimeout(changec,500)
+// function changec(){
+//     x[0].style.fontSize = "52px"
+    // x[0].style.color = "Orange"
+   
+// }
+
+// const g = document.getElementsByTagName("div")
+// console.log(g)
+// g[0].classname = "sai1"
+// g[0].id = "w2"
+// g[1].classname = "sai2"
+// g[1].id = "w1"
+// console.log(g[0])
+// console.log(g[1])
+
+
+// 1)  Access the elements(there some methods which allow us 
+//     to access the html elements) 
+//     2) you can manipulate by using some functionalities.(
+//         There are some methods which allow us to modify 
+//     )
+//     */
+    /*
+    document
+    Accessing methods 
+    i)  getElementById("id") -> returns an object 
+    ii) getElementsByTagName("b") -> return an array
+    iii) getElementsByClassName("className") -> return ans array
+    let document = {
+        getElementById: (id) => {
+            return obj ;
+        },
+        getElementsByTagName: ("tagname") => {
+            return [] ;
+        }
+    
+    }
+    let  htmlElement = {
+        id :"test" ,
+        className: "test1"
+    }
+    */ 
+    // getElementById("id")
+    /* let htmlElement = {
+        
+    }
+    */
+    
+// 1) upon dblclicking show the like icons
+// 2) after clicking a button you need to show some form 
+// that form need be removed after 5 seconds 
+// 3) you need to have a button inside a box if you click on 
+// that button that box wherein the button is present need 
+// to be removed .
